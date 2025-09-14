c584124e0ccb3de0cb0006c1773c10710d0c5bdf
Michal <michal.zaczek@interia.pl>
2025-09-14 21:18:50 +0200

    main.py: def get_book_text

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+
+def main():
+    frankenstein_text = get_book_text("books/frankenstein.txt")
+    print(frankenstein_text)
+
+
+main()
