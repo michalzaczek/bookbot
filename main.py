a1e33a82de9619d683b5d4bcaebcc35ae2c3996f
Michal <michal.zaczek@interia.pl>
2025-09-14 22:07:40 +0200

    main.py: use sys.argv for custom path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_characters, get_num_words, get_sorted_letters
+import sys
 
 
 def get_book_text(file_path):
@@ -7,7 +8,12 @@ def get_book_text(file_path):
 
 
 def main():
-    BOOK_FILE_PATH = "books/frankenstein.txt"
+    # BOOK_FILE_PATH = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    BOOK_FILE_PATH = sys.argv[1]
 
     frankenstein_text = get_book_text(BOOK_FILE_PATH)
     num_words = get_num_words(frankenstein_text)
